c2ab01e10dc05bf13e46c9aa6f5c7c2aa5759f96
Devin <devinazzie@gmail.com>
2020-04-16 14:20:22 +0200

    Completed Module 3

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def welcome(request):
+    return HttpResponse("Welcome to the Meeting Planner!")
+
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("Hi, I'm Devin Azzie and you're watching Disney Channel")
+
